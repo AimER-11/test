e78a31306829c258df1422c0333f00975a2c98a6
Aki <idkaki96@gmail.com>
2026-08-12 15:50:01 +0530

    Deepak

---
+++ dashboard.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Deepak!" << endl;
+    cout << "Hello, Ayush!" << endl;
+    cout << "Hello, Sahill!" << endl;
+    return 0;
+}
